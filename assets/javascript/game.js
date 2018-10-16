b2a37d8a47bdce1cd56bb01bf44c0ceca4afb785
row_gregory <rowgregory@gmail.com>
2018-10-16 17:12:13 -0400

    fixed divs to fit height of screen and removed border

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,141 +1,122 @@
 $(document).ready(function () {
 
-var random_result;
-var playerTotal = 0;
-var lost = 0;
-var win = 0;
+    var random_result;
+    var playerTotal = 0;
+    var lost = 0;
+    var win = 0;
 
-$('.dinoC').on('click', function() {
-    $('#_words').toggle();
-})
-
-
-// function to create game
-var resetAndStart = function () {
-
-    // empying crystal
-    $(".crystals").empty();
-
-    // generating new number
-    random_result = Math.floor(Math.random() * 69) + 30;
-
-    // adding to DOM
-    $("#result").html("Number to Match: " + random_result);
-
-    // looping 4 times
-    for (var i = 0; i < 4; i++) {
-
-        // creating a random number each time
-        var random = Math.floor(Math.random() * 11) + 1;
-
-        // creating div for random number
-        var crystal = $("<button>");
-
-        // giving div attributes
-        crystal.attr({
-            "class": "crystal",
-            "data-random": random
-        });
-
-        // adding to DOM
-        //crystal.html(random);
-
-        // crystal where we are sending everything back
-        $(".crystals").append(crystal);
-    }
-
-    $("#totalScore").html("Total score: " + playerTotal);
-
-
-}
-
-resetAndStart();
-
-
-// event delegation: 
-// when page loads it replaces div with new elements and starts listening to the DOM and not the old div
-$(document).on("click", ".crystal", function () {
-
-    var num = parseInt($(this).attr("data-random"));
-
-    playerTotal += num;
-
-    $("#totalScore").html("Total score: " + playerTotal);
-    document.getElementById("footStep").play();
-
-    console.log(playerTotal);
-
-
-    if (playerTotal > random_result) {
-
-        lost++;
-
-        $("#lost").html("Losses: " + lost);
-
-        playerTotal = 0;
-
-        document.getElementById("scream").play();
-
-        resetAndStart();
-    }
-    else if (playerTotal === random_result) {
-
-        win++;
-
-        $("#win").html("Wins: " + win);
-
-        playerTotal = 0;
-
-        document.getElementById("growl").play();
-
-        resetAndStart();
-    }
-
-});
-
-
+    $('.dinoC').on('click', function () {
+        $('#_words').toggle();
+    })
 
     
-    var switchOp = 0;
+    
+    
 
-    $("#_button1").on("click", function () {
+    // function to create game
+    var resetAndStart = function () {
 
-        if(switchOp === 0) {
-            $("#_directions").toggle(1000);
-            $("#_sign").animate({ height: "80vw" });
-            switchOp = 1; 
-            $("#_words").slideToggle(); 
+        // empying crystal
+        $(".crystals").empty();
+
+        // generating new number
+        random_result = Math.floor(Math.random() * 69) + 30;
+
+        // adding to DOM
+        $("#result").html("Number to Match: " + random_result);
+
+        // looping 4 times
+        for (var i = 0; i < 4; i++) {
+
+            // creating a random number each time
+            var random = Math.floor(Math.random() * 11) + 1;
+
+            // creating div for random number
+            var crystal = $("<button>");
+
+            // giving div attributes
+            crystal.attr({
+                "class": "crystal",
+                "data-random": random,
+                "data-index": i, 'id' : 'animate' + i
+            });
+
+            // adding to DOM
+            //crystal.html(random);
+
+            // crystal where we are sending everything back
+            $(".crystals").append(crystal);
         }
-        else {
-            $("#_words").toggle(1000);
-            $("#_sign").animate({ height: "60vw" });
-            $("#_directions").slideToggle();
-            switchOp = 0;
-            // resetBox();
+
+        $("#totalScore").html("Total score: " + playerTotal);
+    }
+
+    resetAndStart();
+
+    
+
+    // event delegation: 
+    // when page loads it replaces div with new elements and starts listening to the DOM and not the old div
+    $(document).on("click", ".crystal", function () {
+
+        var num = parseInt($(this).attr("data-random"));
+
+        playerTotal += num;
+
+        $("#totalScore").html("Total score: " + playerTotal);
+        document.getElementById("footStep").play();
+
+        console.log(playerTotal);
+
+
+        if (playerTotal > random_result) {
+
+            lost++;
+
+            $("#lost").html("Losses: " + lost);
+
+            playerTotal = 0;
+
+            document.getElementById("scream").play();
+
+            resetAndStart();
         }
+        else if (playerTotal === random_result) {
 
-        
+            win++;
 
+            $("#win").html("Wins: " + win);
+
+            playerTotal = 0;
+
+            document.getElementById("growl").play();
+
+            resetAndStart();
+        }
     });
-
-
-    // $("#_button1").on("click", function () {
-        // $("#_sign").animate({ height: "600px" });
-
-
-    // });
-    //$("#_button1").on("click", function () {
-        
-    //});
-
-//});
-
-// var resetBox = function () {
-//     $("#_button1").on("click", function () {
-//         $("#_sign").animate({ height: "450px" });
-//     });
-// }
-
 });
+
+
+        // previous animated direction screen before bootstrap was added
+        // reference code for future projects
+
+        // var switchOp = 0;
+
+        // $("#_button1").on("click", function () {
+
+        //     if(switchOp === 0) {
+        //         $("#_directions").toggle(1000);
+        //         $("#_sign").animate({ height: "80vw" });
+        //         switchOp = 1; 
+        //         $("#_words").slideToggle(); 
+        //     }
+        //     else {
+        //         $("#_words").toggle(1000);
+        //         $("#_sign").animate({ height: "60vw" });
+        //         $("#_directions").slideToggle();
+        //         switchOp = 0;
+        //     }
+        // });
+
 
 
